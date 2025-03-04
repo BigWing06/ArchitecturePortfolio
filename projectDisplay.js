19739c41d7fe75a340b679f63525da85985ef108
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2025-03-03 19:20:39 -0500

    Projectdisplay load fix

--- projectDisplay.js
+++ projectDisplay.js
@@ -1,4 +1,10 @@
-const urlString = window.localStorage.search;
+const urlString = window.location.search;
 const urlPrams = new URLSearchParams(urlString);
 const project = urlPrams.get("project");
 console.log(project);
+fetch("projectMain.json")
+  .then(response => response.json())
+  .then(data=>{
+    pageData = data[project];
+    document.getElementById("pageHeader").textContent = pageData.title
+    })
